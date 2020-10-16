3a92cef80d899a3789843733720aeb8f7a6307d8
Opilane TTHK <opilane@tthk.ee>
2020-10-16 12:34:21 +0300

    Add project files.

---
+++ Zakladkadaniel/Jaanuar.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zakladkadaniel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Jaanuar : ContentPage
+    {
+        public Jaanuar()
+        {
+            InitializeComponent();
+            Grid grd = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(2,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(3,GridUnitType.Star)}
+
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)}
+                }
+
+
+            };
+        }
+    }
+}
